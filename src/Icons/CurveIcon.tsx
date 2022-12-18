ae9be81e96736479ed39b7910a8b5d6c1ac76d7d
ochom <ochomrichard752@gmail.com>
2022-12-18 11:58:02 +0300

    draft onboarding screen

---
+++ src/Icons/CurveIcon.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { SvgIconProps } from "./types";
+import { View } from "native-base";
+
+function CurveIcon(props: SvgIconProps) {
+  return <View style={props.style}>CurveIcon</View>;
+}
+
+export default CurveIcon;
--- src/screens/OnBoardingScreen/OnBoardingScreen.tsx
+++ src/screens/OnBoardingScreen/OnBoardingScreen.tsx
@@ -1,37 +1,47 @@
+import { Button, Center, Text, View } from "native-base";
+
+import CurveIcon from "../../Icons/CurveIcon";
+import OnboardingIcon from "../../Icons/OnboardingIcon";
 import React from "react";
 import { SafeArea } from "../../components";
-import RectangleIcon from "../../Icons/RectangleIcon";
-import OnboardingIcon from "../../Icons/OnboardingIcon";
 import { useTranslation } from "react-i18next";
-import { Button, Center, ScrollView, Text } from "native-base";
 
 const OnBoardingScreen = () => {
   const { t } = useTranslation();
   return (
-    <SafeArea style={{ position: "relative" }}>
-      <ScrollView>
-        <RectangleIcon />
-        <OnboardingIcon position="absolute" top={150} left="10%" />
-
-        <Center mt="10">
-          <Text fontSize="titleLarge" fontWeight={600}>
-            {t("welcome")}
-          </Text>
-          <Text
-            maxW="295px"
-            textAlign="center"
-            my="5"
-            fontSize="Button"
-            color="main.gray.4"
-            fontFamily="heading"
-          >
-            {t("welcome_text")}
-          </Text>
-          <Button my="6" maxW="50%" variant="primary">
-            {t("get_started")}
-          </Button>
-        </Center>
-      </ScrollView>
+    <SafeArea>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 2, position: "relative" }}>
+          <CurveIcon style={{ position: "absolute", top: 0, right: 0 }} />
+          <OnboardingIcon
+            style={{
+              position: "absolute",
+              top: "10%",
+              left: "10%",
+            }}
+          />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Center mt="10">
+            <Text fontSize="titleLarge" fontWeight={600}>
+              {t("welcome")}
+            </Text>
+            <Text
+              maxW="295px"
+              textAlign="center"
+              my="5"
+              fontSize="Button"
+              color="main.gray.4"
+              fontFamily="heading"
+            >
+              {t("welcome_text")}
+            </Text>
+            <Button my="6" maxW="50%" variant="primary">
+              {t("get_started")}
+            </Button>
+          </Center>
+        </View>
+      </View>
     </SafeArea>
   );
 };
